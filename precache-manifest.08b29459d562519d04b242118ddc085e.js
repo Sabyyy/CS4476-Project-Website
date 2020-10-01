d972b957f1d8f0313bd43b70087da688c0b6a505
bqin628 <bruce.qin@outlook.com>
2020-10-01 14:06:12 -0700

    Updates

---
+++ precache-manifest.08b29459d562519d04b242118ddc085e.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1490263832b3ccffb1a00a10950dcefe",
+    "url": "/CS4476-Project-Website/index.html"
+  },
+  {
+    "revision": "e3b317deb844b263b5d6",
+    "url": "/CS4476-Project-Website/static/css/main.1aa5d802.chunk.css"
+  },
+  {
+    "revision": "b963109e45c3ba885880",
+    "url": "/CS4476-Project-Website/static/js/2.1018e24f.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/CS4476-Project-Website/static/js/2.1018e24f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b317deb844b263b5d6",
+    "url": "/CS4476-Project-Website/static/js/main.aff59b3a.chunk.js"
+  },
+  {
+    "revision": "c251be4e9842592093e0098333b51410",
+    "url": "/CS4476-Project-Website/static/js/main.aff59b3a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf65e0078cb47f0b78b8",
+    "url": "/CS4476-Project-Website/static/js/runtime-main.39d87ec7.js"
+  },
+  {
+    "revision": "c497048c159e34edd80614346ff381d3",
+    "url": "/CS4476-Project-Website/static/media/bruce.c497048c.jpeg"
+  },
+  {
+    "revision": "77f5794e2eb49f7989b8f85e92cfa4e0",
+    "url": "/CS4476-Project-Website/static/media/filler.77f5794e.png"
+  },
+  {
+    "revision": "2892f654a5257b626007fd14cda5547d",
+    "url": "/CS4476-Project-Website/static/media/yiqin.2892f654.jpg"
+  }
+]);
